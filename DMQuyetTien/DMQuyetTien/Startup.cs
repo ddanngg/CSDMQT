600298591784b650af6cf50aab4cb32596b89999
US <andinh2601@gmail.com>
2018-11-21 10:49:40 +0700

    empty project

---
+++ DMQuyetTien/DMQuyetTien/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(DMQuyetTien.Startup))]
+namespace DMQuyetTien
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
