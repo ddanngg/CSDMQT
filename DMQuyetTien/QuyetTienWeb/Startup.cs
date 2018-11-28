1913ce8a485448615482a4b55798cfda390bdec7
andinh <andinh2601@gmail.com>
2018-11-28 11:51:05 +0700

    an

---
+++ DMQuyetTien/QuyetTienWeb/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(QuyetTienWeb.Startup))]
+namespace QuyetTienWeb
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
