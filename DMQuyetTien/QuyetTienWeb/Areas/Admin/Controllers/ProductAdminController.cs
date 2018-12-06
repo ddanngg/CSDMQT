3832bd51984831b6baa8370c67c0e5657ef8e5b2
an <andinh2601@gmail.com>
2018-12-06 12:18:13 +0700

    admin front-end

---
+++ DMQuyetTien/QuyetTienWeb/Areas/Admin/Controllers/ProductAdminController.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using QuyetTienWeb.Models;
+using System.Transactions;
+
+namespace QuyetTienWeb.Areas.Admin.Controllers
+{
+    public class ProductAdminController : Controller
+    {
+        CS4PEEntities db = new CS4PEEntities();
+        //
+        // GET: /Admin/ProductAdmin/
+        public ActionResult Index()
+        {
+            var product = db.BangSanPhams.OrderByDescending(x => x.id).ToList();
+            return View(product);
+        }
+        // GET: /Admin/AdminProduct/Create
+        public ActionResult Create()
+        {
+            ViewBag.Loai_id = new SelectList(db.LoaiSanPhams, "id", "TenLoai");
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(BangSanPham model)
+        {
+            CheckBangSanPham(model);
+            if (ModelState.IsValid)
+            {
+                using (var scope = new TransactionScope())
+                {
+                    db.BangSanPhams.Add(model);
+                    db.SaveChanges();
+
+                    var path = Server.MapPath("~/App_Data");
+                    path = path + "/" + model.id;
+                    if (Request.Files["HinhAnh"] != null && Request.Files["HinhAnh"].ContentLength > 0)
+                    {
+                        Request.Files["HinhAnh"].SaveAs(path);
+                        scope.Complete();
+                        return RedirectToAction("Index");
+                    }
+                    else
+                    {
+                        ModelState.AddModelError("HinhAnh", "Chưa có hình ảnh sản phẩm");
+                    }
+                }
+            }
+
+            ViewBag.Loai_id = new SelectList(db.LoaiSanPhams, "id", "TenLoai", model.Loai_id);
+            return View(model);
+        }
+
+        private void CheckBangSanPham(BangSanPham model)
+        {
+            if (model.GiaGoc < 0)
+            {
+                ModelState.AddModelError("GiaGoc", "Giá gốc phải lớn hơn 0");
+            }
+            if (model.GiaBan < 0)
+            {
+                ModelState.AddModelError("GiaBan", "Giá bán phải lớn hơn 0");
+            }
+            if (model.GiaGop < 0)
+            {
+                ModelState.AddModelError("GiaGop", "Giá trả góp phải lớn hơn 0");
+            }
+        }
+	}
+}
