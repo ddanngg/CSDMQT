6bf7be01f14f769d04eda81572749d729551d9a9
an <andinh2601@gmail.com>
2018-12-12 11:23:49 +0700

    dmmoq

--- DMQuyetTien/QuyetTienWeb.Tests/Controllers/BangSanPhamTest.cs
+++ DMQuyetTien/QuyetTienWeb.Tests/Controllers/BangSanPhamTest.cs
@@ -5,6 +5,9 @@ using QuyetTienWeb.Controllers;
 using System.Web.Mvc;
 using QuyetTienWeb.Models;
 using System.Collections.Generic;
+using System.Web;
+using System.Web.Routing;
+using Moq;
 
 
 namespace QuyetTienWeb.Tests.Controllers
@@ -23,6 +26,31 @@ namespace QuyetTienWeb.Tests.Controllers
             Assert.IsInstanceOfType(result.Model, typeof(List<BangSanPham>));
             Assert.AreEqual(db.BangSanPhams.Count(), ((List<BangSanPham>)result.Model).Count);
 
+        }
+
+        [TestMethod]
+        public void TestCreate1()
+        {
+            var controller = new BangSanPhamController();
+            var result = controller.Create() as ViewResult;
+
+            Assert.IsNotNull(result);
+            Assert.IsInstanceOfType(result.ViewData["Loai_id"], typeof(SelectList));
+        }
+
+        [TestMethod]
+        public void TestDetails()
+        {
+            var controller = new BangSanPhamController();
+            var context = new Mock<HttpContextBase>();
+            context.Setup(c => c.Server.MapPath("~/App_Data/0")).Returns("~/App_Data/0");
+            controller.ControllerContext = new ControllerContext(context.Object, new RouteData(), controller);
+
+            var result = controller.Details("0") as FilePathResult;
+
+            Assert.IsNotNull(result);
+            Assert.AreEqual("images", result.ContentType);
+            Assert.AreEqual("~/App_Data/0", result.FileName);
         }
     }
 }
