d0e81da04ec9a1895dbb71aee5256dc56e438678
an <andinh2601@gmail.com>
2018-12-12 11:57:33 +0700

    unit test

--- DMQuyetTien/QuyetTienWeb.Tests/Controllers/BangSanPhamTest.cs
+++ DMQuyetTien/QuyetTienWeb.Tests/Controllers/BangSanPhamTest.cs
@@ -8,6 +8,8 @@ using System.Collections.Generic;
 using System.Web;
 using System.Web.Routing;
 using Moq;
+using System.Transactions;
+
 
 
 namespace QuyetTienWeb.Tests.Controllers
@@ -51,6 +53,45 @@ namespace QuyetTienWeb.Tests.Controllers
             Assert.IsNotNull(result);
             Assert.AreEqual("images", result.ContentType);
             Assert.AreEqual("~/App_Data/0", result.FileName);
+        }
+
+        [TestMethod]
+        public void TestCreate2()
+        {
+            var controller = new BangSanPhamController();
+            var context = new Mock<HttpContextBase>();
+            var request = new Mock<HttpRequestBase>();
+            var files = new Mock<HttpFileCollectionBase>();
+            var file = new Mock<HttpPostedFileBase>();
+            context.Setup(c => c.Request).Returns(request.Object);
+            request.Setup(r => r.Files).Returns(files.Object);
+            files.Setup(f => f["HinhAnh"]).Returns(file.Object);
+            file.Setup(f => f.ContentLength).Returns(1);
+            context.Setup(c => c.Server.MapPath("~/App_Data")).Returns("~/App_Data");
+            controller.ControllerContext = new ControllerContext(context.Object, new RouteData(), controller);
+
+            var db = new CS4PEEntities();
+            var model = new BangSanPham();
+            model.Loai_id = db.LoaiSanPhams.First().id;
+            model.MaSP = "MaSP";
+            model.TenSP = "TenSP";
+            model.GiaGoc = 123;
+            model.GiaBan = 456;
+            model.GiaGop = 789;
+            model.SoLuongTon = 10;
+
+            using (var scope = new TransactionScope())
+            {
+                var result0 = controller.Create(model) as RedirectToRouteResult;
+                Assert.IsNotNull(result0);
+                file.Verify(f => f.SaveAs(It.Is<string>(s => s.StartsWith("~/App_Data/"))));
+                Assert.AreEqual("Index", result0.RouteValues["action"]);
+
+                file.Setup(f => f.ContentLength).Returns(0);
+                var result1 = controller.Create(model) as ViewResult;
+                Assert.IsNotNull(result1);
+                Assert.IsInstanceOfType(result1.ViewData["Loai_id"], typeof(SelectList));
+            }
         }
     }
 }
